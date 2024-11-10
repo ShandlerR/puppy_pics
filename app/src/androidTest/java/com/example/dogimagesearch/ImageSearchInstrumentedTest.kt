e0b63401da7b8f89983ec4e2d4facd8643d6a955
Shandler Rechenberger <83612169+ShandlerR@users.noreply.github.com>
2024-11-10 11:10:03 -0500

    first UI Test Pass

--- app/src/androidTest/java/com/example/dogimagesearch/ImageSearchInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/dogimagesearch/ImageSearchInstrumentedTest.kt
@@ -1,21 +1,41 @@
 package com.example.dogimagesearch
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.example.dogimagesearch.ui.theme.DogImageSearchTheme
 import org.junit.Rule
+import org.junit.Test
 
 class ImageSearchInstrumentedTest {
     @get:Rule
     val composeTestRule = createComposeRule()
+    val directory = mapOf(
+        "khaleesi" to listOf(
+            R.drawable.kahlsee0,
+            R.drawable.kahlsee1,
+            R.drawable.kahlsee2,
+            R.drawable.kahlsee3
+        ),
+        "qubert" to listOf(
+            R.drawable.qbert0,
+            R.drawable.qbert1
+        )
+    )
 
     // Cover the big stuff
 
+    @Test
     fun noSearch() {
         //setup
-
+        composeTestRule.setContent {
+            DogImageSearchTheme {
+                DogImageSearch(directory)
+            }
+        }
         //exercise
-
         //verify
-
+        composeTestRule.onNodeWithText("Khaleesi")
+            .assertExists("No Node with this text was found")
         //teardown
     }
 
